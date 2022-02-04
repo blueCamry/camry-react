f9c7f71484681aaf17b945163535719f96a3d9d9
EC2 Default User <ec2-user@ip-172-31-66-90.ec2.internal>
2022-02-04 19:26:05 +0000

    new for V3

--- src/App.js
+++ src/App.js
@@ -3,20 +3,6 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-	  return (
-		      <div className="App">
-		        <header className="App-header">
-		          <img src={logo} className="App-logo" alt="logo" />
-		          <h1>Hello from V2</h1>
-		        </header>
-		      </div>
-		    );
-}
-
-export default App;
-import './App.css';
-
-function App() {
   return (
     <div className="App">
       <header className="App-header">
